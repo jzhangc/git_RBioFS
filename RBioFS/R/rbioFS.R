eb1c76b2dffdbd77744f3ca60ad9d1a5f26107ed
Jing Zhang <jingzhang@Jings-MBP-mid-2012.local>
2016-12-06 18:36:08 -0500

    0.2.1 bug fixes

--- RBioFS/R/rbioFS.R
+++ RBioFS/R/rbioFS.R
@@ -13,16 +13,16 @@
 #' @param SFS_mTry Number of randomly selected featurs for constructing trees for SFS-like FS step. When \code{"recur_default"}, it'll be based on \code{p / 3}; when \code{"rf_default"}, it will use the default setting in \code{randomForest} package. Default is \code{"recur_default"}.
 #' @param multicore If to use parallel computing. Default is \code{TRUE}.
 #' @param plot If to plot results, bar graph for initial FS and joint-point curve for SFS-like FS. Default is \code{TRUE}
-#' @param initalFS_Title Figure title. Make sure to use quotation marks. Use \code{NULL} to hide. Default is \code{NULL}.
-#' @param initalFS_xLabel X-axis label. Make sure to use quotation marks. Use \code{NULL} to hide. Default is \code{NULL}.
-#' @param initalFS_yLabel Y-axis label. Make sure to use quotatio marks. Use \code{NULL} to hide. Default is \code{"Mean Decrease in Accurac"}
-#' @param initalFS_n Number of features to show. Takes integer numbers. Default is \code{"all"} (make sure to include quotation marks).
-#' @param initalFS_errorbar The type of errorbar in the graph. Options are \code{"SEM"} (standard error of the mean) or \code{"SD"} (standard deviation). Default is \code{"SEM"}.
-#' @param initalFS_errorbarWidth The width of the errorbar. Default is \code{0.2}.
-#' @param initalFS_xTxtSize Font size for the x-axis text. Default is \code{10}.
-#' @param initalFS_yTxtSize Font size for the y-axis text. Default is \code{10}.
-#' @param initalFS_plotWidth The width of the figure for the final output figure file. Default is \code{170}.
-#' @param initalFS_plotHeight The height of the figure for the final output figure file. Default is \code{150}.
+#' @param initialFS_Title Figure title. Make sure to use quotation marks. Use \code{NULL} to hide. Default is \code{NULL}.
+#' @param initialFS_xLabel X-axis label. Make sure to use quotation marks. Use \code{NULL} to hide. Default is \code{NULL}.
+#' @param initialFS_yLabel Y-axis label. Make sure to use quotatio marks. Use \code{NULL} to hide. Default is \code{"Mean Decrease in Accurac"}
+#' @param initialFS_n Number of features to show. Takes integer numbers. Default is \code{"all"} (make sure to include quotation marks).
+#' @param initialFS_errorbar The type of errorbar in the graph. Options are \code{"SEM"} (standard error of the mean) or \code{"SD"} (standard deviation). Default is \code{"SEM"}.
+#' @param initialFS_errorbarWidth The width of the errorbar. Default is \code{0.2}.
+#' @param initialFS_xTxtSize Font size for the x-axis text. Default is \code{10}.
+#' @param initialFS_yTxtSize Font size for the y-axis text. Default is \code{10}.
+#' @param initialFS_plotWidth The width of the figure for the final output figure file. Default is \code{170}.
+#' @param initialFS_plotHeight The height of the figure for the final output figure file. Default is \code{150}.
 #' @param SFS_xLabel X-axis label. Make sure to use quotation marks. Use \code{NULL} to hide. Default is \code{NULL}.
 #' @param SFS_yLabel Y-axis label. Make sure to use quotatio marks. Use \code{NULL} to hide. Default is \code{"Mean Decrease in Accurac"}
 #' @param SFS_n Number of features to show. Takes integer numbers. Default is \code{"all"} (make sure to include quotation marks).
@@ -33,7 +33,7 @@
 #' @param SFS_yTxtSize Font size for the y-axis text. Default is \code{10}.
 #' @param SFS_plotWidth The width of the figure for the final output figure file. Default is \code{170}.
 #' @param SFS_plotHeight The height of the figure for the final output figure file. Default is \code{150}.
-#' @return Outputs two \code{list} objects and \code{.csv} for the two FS steps. And, if \code{plot = TRUE}, a bargraph for inital FS and joint-point curve for SFS-like FS step. A \code{.csv} file with imputed data is also generated if \code{impute = TRIE}.
+#' @return Outputs two \code{list} objects and \code{.csv} for the two FS steps. And, if \code{plot = TRUE}, a bargraph for initial FS and joint-point curve for SFS-like FS step. A \code{.csv} file with imputed data is also generated if \code{impute = TRIE}.
 #' @details Make sure to arrange input \code{.csv} file with first two columns for smaple ID and conditions, and the rest for features (e.g., genes).
 #' @examples
 #' \dontrun{
@@ -45,11 +45,11 @@ rbioFS <- function(file, impute = FALSE, imputeMethod = "rf", imputeIter = 10, i
                    nTimes = 50, nTree = 1001, SFS_mTry = "recur_default",
                    multicore = TRUE,
                    plot = TRUE,
-                   initalFS_n = "all",
-                   initalFS_Title = NULL, initalFS_xLabel = "Mean Decrease in Accuracy", initalFS_yLabel = NULL,
-                   initalFS_errorbar = "SEM", initalFS_errorbarWidth = 0.2,
-                   initalFS_xTxtSize = 10, initalFS_yTxtSize =10,
-                   initalFS_plotWidth = 170, initalFS_plotHeight = 150,
+                   initialFS_n = "all",
+                   initialFS_Title = NULL, initialFS_xLabel = "Mean Decrease in Accuracy", initialFS_yLabel = NULL,
+                   initialFS_errorbar = "SEM", initialFS_errorbarWidth = 0.2,
+                   initialFS_xTxtSize = 10, initialFS_yTxtSize =10,
+                   initialFS_plotWidth = 170, initialFS_plotHeight = 150,
                    SFS_n = "all",
                    SFS_Title = NULL, SFS_xLabel = NULL, SFS_yLabel = NULL,
                    SFS_errorbar = "SEM", SFS_errorbarWidth = 0.2,
@@ -83,16 +83,16 @@ rbioFS <- function(file, impute = FALSE, imputeMethod = "rf", imputeIter = 10, i
 
     RBioFS::rbioRF_initialFS(objTitle = substr(noquote(file), 1, nchar(file) - 4), x = input, targetVar = tgt,
                              nTimes = nTimes, nTree = nTree,
-                             plot = TRUE, n = initalFS_n,
-                             Title = initalFS_Title,
-                             errorbar = initalFS_errorbar, errorbarWidth = initalFS_errorbarWidth,
-                             xLabel = initalFS_xLabel, yLabel = initalFS_yLabel,
-                             xTxtSize = initalFS_xTxtSize, yTxtSize = initalFS_yTxtSize,
-                             plotWidth = initalFS_plotWidth, plotHeight = initalFS_plotHeight) # initial FS
+                             plot = TRUE, n = initialFS_n,
+                             Title = initialFS_Title,
+                             errorbar = initialFS_errorbar, errorbarWidth = initialFS_errorbarWidth,
+                             xLabel = initialFS_xLabel, yLabel = initialFS_yLabel,
+                             xTxtSize = initialFS_xTxtSize, yTxtSize = initialFS_yTxtSize,
+                             plotWidth = initialFS_plotWidth, plotHeight = initialFS_plotHeight) # initial FS
 
 
     RBioFS::rbioRF_SFS(objTitle = substr(noquote(file), 1, nchar(file) - 4),
-                       x = get(paste(substr(noquote(file), 1, nchar(file) - 4), "_inital_FS", sep = ""))$matrix_initial_FS,
+                       x = get(paste(substr(noquote(file), 1, nchar(file) - 4), "_initial_FS", sep = ""))$matrix_initial_FS,
                        targetVar = tgt, nTimes = nTimes, mTry = SFS_mTry,
                        plot = TRUE,
                        n = SFS_n,
@@ -111,7 +111,7 @@ rbioFS <- function(file, impute = FALSE, imputeMethod = "rf", imputeIter = 10, i
 
 
     RBioFS::rbioRF_SFS(objTitle = substr(noquote(file), 1, nchar(file) - 4),
-                       x = get(paste(substr(noquote(file), 1, nchar(file) - 4), "_inital_FS", sep = ""))$matrix_initial_FS,
+                       x = get(paste(substr(noquote(file), 1, nchar(file) - 4), "_initial_FS", sep = ""))$matrix_initial_FS,
                        targetVar = tgt, nTimes = nTimes, mTry = SFS_mTry,
                        plot = FALSE) # SFS
 
